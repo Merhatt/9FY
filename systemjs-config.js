a986b92fc8734040360ca896debf0b873fd198ca
gyankov <g.yankov951@abv.bg>
2016-09-24 10:41:07 +0300

    Added few features

---
+++ systemjs-config.js
@@ -0,0 +1,9 @@
+SystemJS.config({
+    transpiler: 'plugin-babel',
+
+    map: {
+        'plugin-babel': './node_modules/systemjs-plugin-babel/plugin-babel.js',
+        'systemjs-babel-build': './node_modules/systemjs-plugin-babel/systemjs-babel-browser.js',
+       
+    }
+});
