44618904fad591834d983503c21560a8873eb426
grozdanova <a.grozdanova90@gmail.com>
2016-09-28 16:43:52 +0300

    Add class Validator

--- public/js/data.js
+++ public/js/data.js
@@ -2,6 +2,30 @@
 import {Requester} from 'requester'
 import {KINVEY} from 'constants'
 import 'jquery'
+class Validator {
+    static validateUser(user){
+        if (typeof user !== 'string') {
+            throw new Error('User must be a text!');
+        }
+        if (typeof user === undefined) {
+            throw new Error('Must be a some text!');
+        }
+        if (!user) {
+            throw new Error('It must be a not empty text!');
+        }
+        if (user.length < 3 || user.length > 20) {
+            throw new Error('Username must be between 3 and 20 characters!');
+        }
+    }
+    static validatePassword(password){
+        if (typeof password === undefined) {
+            throw new Error('You must write your password!');
+        }
+        if (!password) {
+            throw new Error('You must write your password!');
+        }
+    }
+}
 class Data {
     static register(user) {
         let url = `https://baas.kinvey.com/user/${KINVEY.appId}/`;
