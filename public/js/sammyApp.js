cab94f6be31e933d16c085dfbeb9312042e7dad2
gyankov <g.yankov951@abv.bg>
2016-09-30 15:10:16 +0300

    Font awesome styles added

---
+++ public/js/sammyApp.js
@@ -0,0 +1,31 @@
+/// <reference path="../../jquery.d.ts"/>
+import Sammy from 'sammy';
+import 'jquery';
+import { userAction } from 'userActions';
+
+const content = '#content';
+
+var sammyApp = new Sammy(content, function () {
+    this.get('#/', userAction.home);
+    this.get('#/register', userAction.register);
+    this.get('#/login', userAction.login);
+    this.get('#/logout', userAction.logout);
+    this.get('#/fresh', userAction.fresh);
+    this.get('#/fresh/:id', function () {
+        userAction.song(this.params.id, 'fresh');
+    });
+    this.get('#/trending', userAction.trending);
+    this.get('#/trending/:id', function () {
+        userAction.song(this.params.id, 'trending');
+    });
+    this.get('#/hot', userAction.hot);
+    this.get('#/hot/:id', function () {
+        userAction.song(this.params.id, 'hot');
+    });
+    this.get('#/favorite', userAction.favorites);  
+});
+
+
+$(function () {
+    sammyApp.run('#/');
+});
